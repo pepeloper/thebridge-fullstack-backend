46463da1eb5c8d6ec3f067e3808547921bece94b
Pepe <pgarciag93@gmail.com>
2024-11-21 13:34:41 +0100

    feat: init routes

--- express/main.js
+++ express/main.js
@@ -4,7 +4,23 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Bienvenido a mi API");
+});
+
+app.get("/events", (req, res) => {
+  res.send("Listado de eventos");
+});
+
+app.post("/events", (req, res) => {
+  res.send("Crear un evento");
+});
+
+app.put("/events", (req, res) => {
+  res.send("Actualizar un evento");
+});
+
+app.delete("/events", (req, res) => {
+  res.send("Eliminar un evento");
 });
 
 app.listen(PORT, () => {
